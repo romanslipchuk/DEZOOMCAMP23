7bcdcd0e52715c471667b81372196ccd1eaf08f5
Roman Slipchuk <roman.slipchuk@gmail.com>
2023-04-21 22:13:37 +0200

    Terraform blows the party

---
+++ week_1_basics_n_setup/1_terraform_gcp/my_dir/projects/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  backend "lo" {}
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("/home/romasl/Documents/LEARN/DE/DEZOOMCAMP23/week_1_basics_n_setup/1_terraform_gcp/my_dir/dtc-de-course-384415-46a806037535.json")
+
+  project = var.project # 
+  region  = var.region # "europe-west4"
+}
+
+resource "google_storage_bucket" "data_lake_bucket" {
+  name = "${local.data_lake_bucket}_${var.project}"
+  location = var.region
+
+  storage_class = var.storage_class
+  uniform_bucket_level_access = 
+  
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30 // days
+    }
+  }
+  force_destroy = true
+}
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = var.BQ_DATASET
+  project = var.project
+  location = var.region
+  
+}
+
---
+++ week_1_basics_n_setup/1_terraform_gcp/my_dir/projects/variables.tf
@@ -0,0 +1,24 @@
+locals {
+  data_lake_bucket = "dtc_data_klake"
+}
+
+variable "project" {
+  description = "dtc-de-course-384415"
+}
+
+variable "region" {
+  description = "europe-west6"
+  default = "europe-west6"
+  type = string
+}
+
+variable "storage_class" {
+  description = "I'll check what type is more preferrable when get to this"
+  default = STANDARD
+}
+
+variable "BQ_DATASET" {
+  description = "Big Querry Dataset will contain raw data"
+  type = string
+  default = "trips_data_all"
+}
